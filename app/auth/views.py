7f11e5bb0b2f9e633de7e2813b093d330183ffa6
ClarisseU <clarisseum@gmail.com>
2019-10-02 14:51:44 +0200

    add function register

---
+++ app/auth/views.py
@@ -0,0 +1,18 @@
+from flask_login import login_required
+from flask import render_template, redirect,url_for
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
+
