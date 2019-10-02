bcaeea2bad28e1c2326a19bb23b60e6c7c6e4663
usanase Francoise <fusanasencoise@gmail.com>
2019-10-02 18:22:49 +0200

    about

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,11 @@ def index():
 
     
     return render_template('index.html')
+@main.route('/about')
+def About():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    
+    return render_template('about.html')
