e6c3bc62b2997faa26052921c5036cbf2e6e276e
ClarisseU <clarisseum@gmail.com>
2019-10-02 14:42:51 +0200

    add user model

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from flask_login import UserMixin
+from . import login_manager
+
+class User(UserMixin, db.Model):
+    __tablename__='users'
+    
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, index = True)
+    password_hash = db.Column(db.String(255))
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
