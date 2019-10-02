496c6873967857bd335e4adaca98642a60f3987c
ClarisseU <clarisseum@gmail.com>
2019-10-02 15:37:59 +0200

    add profile function

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for, abort
 from . import main
+from ..models import User
+
 @main.route('/')
 def index():
     '''
@@ -8,3 +10,12 @@ def index():
 
     
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
