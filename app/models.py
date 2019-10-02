15e7d1ef42f9313c57ce07aad00bfc6580c2201d
ClarisseU <clarisseum@gmail.com>
2019-10-02 15:35:37 +0200

    add password secure

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique = True, index = True)
     password_hash = db.Column(db.String(255))
+    password_secure = db.Column(db.String(255))
     
     @login_manager.user_loader
     def load_user(user_id):
