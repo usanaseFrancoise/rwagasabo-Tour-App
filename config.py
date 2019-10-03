519633d1ab7ddb7c068dd49443d89f3bba43f996
usanase Francoise <fusanasencoise@gmail.com>
2019-10-03 17:25:23 +0200

    design

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
     # MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://clarisse:1234@localhost/tour'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fanny:havugima@localhost/tour'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 # export MOVIE_API_KEY=<Your api key>
 export SECRET_KEY=123
-export MAIL_USERNAME=klaryc4@gmail.com
-export MAIL_PASSWORD=bienvenido
+export MAIL_USERNAME=fanny@gmail.com
+export MAIL_PASSWORD=havugima@2019
 
 python3.6 manage.py run
